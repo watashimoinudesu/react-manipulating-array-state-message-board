ae2e1e6961bdc878d060753f009a9e3cf1967c3b
KIRATI-SX <Kirati_Srising@outlook.co.th>
2026-01-09 14:15:53 +0700

    feat: implement message board functionality with add and delete features

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messages, setMessages] = useState([
+    "Hello all ! This is first message.",
+  ]);
+  const [inputMessage,setInputMessage]=useState('');
+
+  function addText(e){
+    setInputMessage(e.target.value);
+  }
+
+  function addMessage(){
+    if(inputMessage.length===0)return;
+    setMessages([...messages,inputMessage]);
+    setInputMessage('');
+  }
+
+  function deleteMessage(index) {
+    const newMessage = [...messages];
+    newMessage.splice(index, 1);
+    setMessages(newMessage);
+  }
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,16 +31,27 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputMessage}
+            onChange={addText}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addMessage}>Submit</button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+      {messages.length > 0 && (
+        <div className="board">
+          {messages.map((message, index) => (
+            <div className="message">
+              <h1 key={index}>{message}</h1>
+              <button
+                className="delete-button"
+                onClick={() => deleteMessage(index)}
+              >
+                x
+              </button>
+            </div>
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 }
